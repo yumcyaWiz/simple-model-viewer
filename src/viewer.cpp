0c13a8a266b604632abf37fcd63a2055f92a5a6c
yumcyawiz <hashdolf1923@gmail.com>
2021-01-28 11:37:40 +0900

    create window

--- src/viewer.cpp
+++ src/viewer.cpp
@@ -1,9 +1,48 @@
-#include <assimp/postprocess.h>
-#include <assimp/scene.h>
-
-#include <assimp/Importer.hpp>
+#include <iostream>
+//
+#include "glad/glad.h"
+//
+#include "GLFW/glfw3.h"
 
 int main() {
-  Assimp::Importer importer;
+  // initialize glfw
+  if (!glfwInit()) {
+    std::cerr << "failed to initialize GLFW" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // setup window and opengl context
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);  // required for Mac
+  GLFWwindow* window =
+      glfwCreateWindow(512, 512, "simple-model-viewer", nullptr, nullptr);
+  if (!window) {
+    std::cerr << "failed to create window" << std::endl;
+    return EXIT_FAILURE;
+  }
+  glfwMakeContextCurrent(window);
+
+  // initialize glad
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cerr << "failed to initialize glad" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+
+    // rendering
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // swap buffer
+    glfwSwapBuffers(window);
+  }
+
+  // exit
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
   return 0;
 }
