8859ff352e174aa7c6b6f12a014693e861dc63db
yumcyawiz <hashdolf1923@gmail.com>
2021-01-29 21:00:51 +0900

    impl compile shader

--- src/shader.h
+++ src/shader.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <vector>
 
 #include "glad/glad.h"
 #include "glm/glm.hpp"
@@ -14,7 +15,7 @@ class Shader {
  private:
   const std::string vertexShaderFilepath;
   std::string vertexShaderSource;
-  const std::string fragmentShaderSource;
+  const std::string fragmentShaderFilepath;
   std::string fragmentShaderSource;
   GLuint vertexShader;
   GLuint fragmentShader;
@@ -37,6 +38,51 @@ class Shader {
   void compileShader() {
     // compile vertex shader
     vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    vertexShaderSource = fileToString(vertexShaderFilepath);
+    const char* vertexShaderSourceC = vertexShaderSource.c_str();
+    glShaderSource(vertexShader, 1, &vertexShaderSourceC, nullptr);
+    glCompileShader(vertexShader);
+
+    // handle compilation error
+    GLuint success = 0;
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (success == GL_FALSE) {
+      std::cerr << "failed to compile vertex shader" << std::endl;
+
+      GLint logSize = 0;
+      glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &logSize);
+      std::vector<GLchar> errorLog(logSize);
+      glGetShaderInfoLog(vertexShader, logSize, &logSize, errorLog.data());
+      std::string errorLogStr(errorLog.begin(), errorLog.end());
+      std::cerr << errorLogStr << std::endl;
+
+      glDeleteShader(vertexShader);
+      return;
+    }
+
+    // compile fragment shader
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    fragmentShaderSource = fileToString(fragmentShaderFilepath);
+    const char* fragmentShaderSourceC = fragmentShaderSource.c_str();
+    glShaderSource(fragmentShader, 1, &fragmentShaderSourceC, nullptr);
+    glCompileShader(fragmentShader);
+
+    // handle compilation error
+    success = 0;
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (success == GL_FALSE) {
+      std::cerr << "failed to compile vertex shader" << std::endl;
+
+      GLint logSize = 0;
+      glGetShaderiv(fragmentShader, GL_INFO_LOG_LENGTH, &logSize);
+      std::vector<GLchar> errorLog(logSize);
+      glGetShaderInfoLog(fragmentShader, logSize, &logSize, errorLog.data());
+      std::string errorLogStr(errorLog.begin(), errorLog.end());
+      std::cerr << errorLogStr << std::endl;
+
+      glDeleteShader(fragmentShader);
+      return;
+    }
   }
 };
 
